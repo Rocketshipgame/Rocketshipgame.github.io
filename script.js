aca4290ea90b81c2a669bf811373a8eaffb7f6c5
Rocketshipgame <gamerocketship@gmail.com>
2024-11-01 10:51:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -347,22 +347,22 @@ if (isMobile()) {
 }
 
 window.addEventListener('load', () => {
-  if (!isGameStarted) {
-    document.getElementById('gameOver').style.display = 'block';
-    document.getElementById('gameOver').innerHTML = `
-      <p>Welcome to the Game!</p>
-      <button id="startGameButton">Start Game</button>
-    `;
-    
-    gameActive = false;
+  // Show the endgame menu right when the game loads
+  document.getElementById('gameOver').style.display = 'block';
+  document.getElementById('gameOver').innerHTML = `
+    <p>Welcome to the Game!</p>
+    <button id="startGameButton">Start Game</button>
+  `;
+  
+  // Pause the game loop initially
+  gameActive = false;
 
-    document.getElementById('startGameButton').addEventListener('click', () => {
-      document.getElementById('gameOver').style.display = 'none';
-      isGameStarted = true; // Set the flag to true when starting the game
-      gameActive = true;
-      gameLoop();
-    });
-  }
+  // Start the game when the player clicks the Start Game button
+  document.getElementById('startGameButton').addEventListener('click', () => {
+    document.getElementById('gameOver').style.display = 'none'; // Hide the endgame menu
+    gameActive = true; // Set the game to active
+    gameLoop(); // Start the game loop
+  });
 });
 
 
